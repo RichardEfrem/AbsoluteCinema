eb7dddd5a766bf7f7d5b64a8157b0fbfed5225bd
wev <c14220281@john.petra.ac.id>
2024-12-25 16:35:09 +0700

    test2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "uas.c14220270.absolutecinema"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "uas.c14220270.absolutecinema"
